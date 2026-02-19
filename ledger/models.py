a6fcc983f1bb8e8e21b3b48a8a15ec228c87ef37
Justin Edward G. Figuerres <justin.edward.figuerres@student.ateneo.edu>
2026-02-19 23:29:18 +0800

    fixed absolute URLs of Ingredient and Recipe

--- ledger/models.py
+++ ledger/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
-
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
-
+    
     def get_absolute_url(self):
-        return reverse('ingredient', args=[str(self.name)])
+        return reverse('ledger:ingredient_detail', args=[int(self.pk)])
     
     class Meta:
         ordering = ['name']
@@ -24,7 +23,7 @@ class Recipe(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('recipe', args=[str(self.name)])
+        return reverse('ledger:recipe_detail', args=[int(self.pk)])
     
     class Meta:
         ordering = ['name']
