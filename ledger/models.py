94de0f758ba70bdf2c98265fb34e92bca701f946
Justin Edward G. Figuerres <justin.edward.figuerres@student.ateneo.edu>
2026-02-19 17:41:53 +0800

    added .gitignore file and Ingredient, Recipe, and RecipeIngredient models

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ingredient', args=[str(self.name)])
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'ingredient'
+        verbose_name_plural = 'ingredients'
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('recipe', args=[str(self.name)])
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'recipe'
+        verbose_name_plural = 'recipes'
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.IntegerField()
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name="recipe_ingredients"
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name="recipe_ingredients"
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('recipe_ingredient', args=[str(self.name)])
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'recipe ingredient'
+        verbose_name_plural = 'recipe ingredients'
