10ecf30cc1367d0edbd0da7cfdeab74cb1d9d69e
Justin Edward G. Figuerres <justin.edward.figuerres@student.ateneo.edu>
2026-03-02 17:32:05 +0800

    changed bio field to be at least 256 characters

--- ledger/models.py
+++ ledger/models.py
@@ -7,7 +7,10 @@ from django.urls import reverse
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    bio = models.CharField(max_length=255)
+    bio = models.CharField(
+        max_length=512,
+        validators=[MinLengthValidator(256)]
+    )
 
     def __str__(self):
         return self.name
