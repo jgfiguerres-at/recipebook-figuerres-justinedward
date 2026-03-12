3624ba8796d46eba9e827bf7a8d12488ee1913c2
Justin Edward G. Figuerres <justin.edward.figuerres@student.ateneo.edu>
2026-03-12 18:21:53 +0800

    added provisions for images in Recipe detail and created recipe_add.html

--- ledger/views.py
+++ ledger/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Profile, Ingredient, Recipe, RecipeIngredient
+from .models import Ingredient, Recipe, RecipeIngredient, RecipeImage
 
 
 class RecipeListView(ListView):
@@ -14,3 +14,8 @@ class RecipeListView(ListView):
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'ledger/recipe_detail.html' # default value
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["recipe_images"] = RecipeImage.objects.all()
+        return context
