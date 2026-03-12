b19a79499be16b8b86ac72531b59e1b094f1445a
Justin Edward G. Figuerres <justin.edward.figuerres@student.ateneo.edu>
2026-03-12 14:22:09 +0800

    renamed app template folder and removed redundant code

--- ledger/views.py
+++ ledger/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
@@ -7,30 +6,11 @@ from django.views.generic.detail import DetailView
 from .models import Profile, Ingredient, Recipe, RecipeIngredient
 
 
-def recipe_list(request):
-    recipes = Recipe.objects.all()
-
-    return render(request, 'recipebook/recipe_list.html', {
-        'recipes': recipes,
-    })
-
-
-def recipe_detail(request, id):
-    recipe = Recipe.objects.get(pk=id)
-    ingredients = Ingredient.objects.filter(recipe__recipe__name=recipe.name)
-    recipe_ingredients = RecipeIngredient.objects.filter(recipe=recipe)
-
-    return render(request, 'recipebook/recipe_detail.html', {
-        'recipe' : recipe,
-        'ingredients': ingredients,
-    })
-
-
 class RecipeListView(ListView):
     model = Recipe
-    template_name = 'recipebook/recipe_list.html' # default value
+    template_name = 'ledger/recipe_list.html' # default value
 
 
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
-    template_name = 'recipebook/recipe_detail.html' # default value
+    template_name = 'ledger/recipe_detail.html' # default value
