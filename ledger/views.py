80ecaf16d1359b644b399d2bc1ffbf16261e8be6
Justin Edward G. Figuerres <justin.edward.figuerres@student.ateneo.edu>
2026-03-12 21:33:16 +0800

    removed extra whitespace in views.py

--- ledger/views.py
+++ ledger/views.py
@@ -35,7 +35,7 @@ class RecipeImageCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.recipe_id = self.kwargs['pk']
         return super().form_valid(form)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['pk'] = self.kwargs['pk']
